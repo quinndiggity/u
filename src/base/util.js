93234ad6534b92c32e52e5f755f443e3745bbeef
zeo1 <42201801+zeo1@users.noreply.github.com>
2018-08-19 20:11:04 +0800

    new: util.js - each, type check

---
+++ src/base/util.js
@@ -0,0 +1,18 @@
+// better x.forEach for array and string
+e = function (x, fn) {
+  for (let i = 0; i < x.length; i++) {
+    fn(x[i], i)
+  }
+}
+e2 = function (x, fn) {
+  for (let i = 0; i < x.length; i+=2) {
+    fn(x[i], x[i+1], i/2)
+  }
+}
+// is object|string|function|array|integer|number
+iso = x => Object.prototype.toString.call(x) == "[object Object]"
+iss = x => typeof x == 'string'
+isf = x => typeof x == 'function'
+isa = Array.isArray
+isi = Number.isInteger
+isn = x => !isNaN(x)
