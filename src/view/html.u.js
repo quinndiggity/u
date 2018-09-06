07e091e87979a572b7d43c6b29251e2b47d4900d
zeo1 <42201801+zeo1@users.noreply.github.com>
2018-09-06 14:54:07 +0800

    Revert "feat: theme format"
    This reverts commit 2c48fd6c9f01a29cde7533f7aff6d27e68df3dcc.

--- src/view/html.u.js
+++ src/view/html.u.js
@@ -2,8 +2,11 @@ i_text(`(s
 css {
   theme {
     px 1 bs 'solid
-    s [12 16 20 24]
-    c (as #2d2d2d #f2777a #99cc99 #ffcc66 #6699cc #cc99cc #66cccc #d3d0c8 #747369 #f2777a #99cc99 #ffcc66 #6699cc #cc99cc #66cccc #f2f0ec]
+    s0 12 s1 16 s2 20 s3 24
+    c0 '#151515 c1 '#ac4142 c2 '#90a959 c3 '#f4bf75
+    c4 '#6a9fb5 c5 '#aa759f c6 '#75b5aa c7 '#d0d0d0
+    c8 '#505050 c9 '#ac4142 ca '#90a959 cb '#f4bf75
+    cc '#6a9fb5 cd '#aa759f ce '#75b5aa cf '#f5f5f5
   }
   abbr (os
     bg background
